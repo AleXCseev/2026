1c6740ff0b9bfbba950de31aced78ba5041adb40
Konstantin Alekseev <alexcseev1990@gmail.com>
2026-05-15 16:46:49 +0300

    add shirts project

---
+++ shirts/src/js/script.js
@@ -0,0 +1,169 @@
+var landingFunctions = {
+  init: function () {
+    this.initLibraris();
+    // this.time();
+    this.order();
+    // this.faq();
+    // this.quantity();
+  },
+
+  initLibraris: function () {
+    $('[href*="#"]').on("click", function (e) {
+      var fixedOffset = 0;
+      // var cardHeight = $("#order").outerHeight(false);
+      // var windowHeight = $(window).height();
+
+      $("html, body")
+        .stop()
+        .animate({ scrollTop: $(this.hash).offset().top + fixedOffset }, 1000);
+      e.preventDefault();
+    });
+
+    // AOS.init({
+    //   disable: function () {
+    //     if ($(window).width() <= 1080) {
+    //       return true;
+    //     }
+    //     return false;
+    //   },
+    //   once: true,
+    //   duration: 1000,
+    //   offset: 0,
+    // });
+
+    // $(window).resize(function () {
+    //   AOS.refresh();
+    // });
+
+    $("[data-fancybox]").fancybox({
+      loop: true,
+      infobar: false,
+      animationEffect: false,
+      backFocus: false,
+      hash: false,
+    });
+  },
+
+  order: function () {
+    let type = "Чоловіча";
+    let size = "M";
+
+    $(".order__btn-type").click(function () {
+      type = $(this).data("type");
+      $(".order__btn-type").removeClass("active");
+      $(this).addClass("active");
+    });
+
+    $(".order__btn-size").click(function () {
+      size = $(this).data("size");
+      $(".order__btn-size").removeClass("active");
+      $(this).addClass("active");
+    });
+  },
+
+  time: function () {
+    Date.prototype.daysInMonth = function () {
+      return 32 - new Date(this.getFullYear(), this.getMonth(), 32).getDate();
+    };
+
+    if (!String.prototype.padStart) {
+      String.prototype.padStart = function padStart(targetLength, padString) {
+        targetLength = targetLength >> 0;
+        padString = String(typeof padString !== "undefined" ? padString : " ");
+        if (this.length > targetLength) {
+          return String(this);
+        } else {
+          targetLength = targetLength - this.length;
+          if (targetLength > padString.length) {
+            padString += padString.repeat(targetLength / padString.length);
+          }
+          return padString.slice(0, targetLength) + String(this);
+        }
+      };
+    }
+
+    function timer() {
+      function runMultiple(hoursSelector, minutesSelector, secondsSelector, milisecondsSelector) {
+        var d = new Date();
+        var h = String(23 - d.getHours()).padStart(2, "0");
+        var m = String(59 - d.getMinutes()).padStart(2, "0");
+        var s = String(60 - d.getSeconds()).padStart(2, "0");
+        // var ms = String(1000 - d.getMilliseconds()).padStart(3, "0");
+        $(hoursSelector).text(h);
+        $(minutesSelector).text(m);
+        $(secondsSelector).text(s);
+        // $(milisecondsSelector).text(ms)
+      }
+      setInterval(function () {
+        runMultiple(".hours", ".minutes", ".seconds");
+      }, 1000);
+    }
+
+    timer();
+
+    function getDate(plusDays) {
+      var now = new Date();
+      now.setDate(now.getDate() + plusDays);
+      var dayNum = "";
+      if (now.getDate() < 10) {
+        dayNum = "0";
+      }
+      dayNum += now.getDate();
+      var monthNum = "";
+      if (now.getMonth() + 1 < 10) {
+        monthNum = "0";
+      }
+      monthNum += now.getMonth() + 1;
+
+      return dayNum + "." + monthNum + "." + now.getFullYear();
+      // return dayNum + "." + monthNum + "." + String(now.getFullYear()).substr(String(now.getFullYear()).length - 2);
+    }
+
+    // $(".date__1").text(getDate(-5));
+    $(".date").text(getDate(-2));
+  },
+
+  faq: function () {
+    $(".faq__btn").click(function () {
+      if ($(this).hasClass("active")) {
+        $(this).closest(".faq__item").find(".faq__btn").removeClass("active");
+        $(this).closest(".faq__item").find(".faq__text").slideUp(300);
+      } else {
+        $(this).addClass("active");
+        $(this).closest(".faq__item").find(".faq__text").slideDown(300);
+      }
+    });
+  },
+
+  quantity: function () {
+    var currentNumber;
+
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    if (localStorage.getItem("quantity")) {
+      $(".quantity").text(localStorage.getItem("quantity"));
+    } else {
+      currentNumber = 25;
+      localStorage.setItem("quantity", currentNumber);
+      $(".quantity").text(currentNumber);
+    }
+
+    setInterval(function () {
+      currentNumber = localStorage.getItem("quantity");
+      if (currentNumber >= 3) {
+        currentNumber = currentNumber - getRandomInt(3);
+        $(".quantity").text(currentNumber);
+        localStorage.setItem("quantity", currentNumber);
+      } else {
+        currentNumber = 25;
+        localStorage.setItem("quantity", currentNumber);
+      }
+    }, 100000);
+  },
+};
+
+$(document).ready(function () {
+  landingFunctions.init();
+});
